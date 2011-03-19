c0b7e308f9b8cf7bde19d55122743b2e8f98fb57
Pascal Deschenes <pdeschen@rassemblr.com>
2011-03-19 16:10:06 -0400

    new message

--- src/hop.groovy
+++ src/hop.groovy
@@ -1,5 +1,7 @@
-answer()
-await(2)
-say("Hello Asian Runner! Welcome to, Jay Pay personal line.");
+answer();
+await(2);
+say("Hello Asian Runner!);
+say("Welcome to, Jay Pay");
+say("Personal line.");
 say("Transferring you call to C I B C. Please wait.");
 transfer("+18004654653");
